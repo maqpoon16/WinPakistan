5e6917df19d0675a90317232322424106c8f32d9
Maqpoon <husssain.km@gmail.com>
2021-09-23 10:38:39 +0500

    23-09-2021

--- app/src/main/java/com/win/pakistan/Models/WalletHistoryDataclass.java
+++ app/src/main/java/com/win/pakistan/Models/WalletHistoryDataclass.java
@@ -1,7 +1,6 @@
 package com.win.pakistan.Models;
 
 public class WalletHistoryDataclass {
-<<<<<<< HEAD
    String historyTimedate,historyTitle,historyPaymentBy,historyCoinOrCash;
    int historyimage;
 
@@ -43,56 +42,8 @@ public class WalletHistoryDataclass {
 
     public void setHistoryCoinOrCash(String historyCoinOrCash) {
         this.historyCoinOrCash = historyCoinOrCash;
-=======
-   String historyluckydate,historyluckyname,historypaidby,historyamount_or_coins;
-   int historyimage;
+        String historyluckydate, historyluckyname, historypaidby, historyamount_or_coins;
+        int historyimage;
 
-    public WalletHistoryDataclass(String historyluckydate, String historyluckyname, String historypaidby, String historyamount_or_coins, int historyimage) {
-        this.historyluckydate = historyluckydate;
-        this.historyluckyname = historyluckyname;
-        this.historypaidby = historypaidby;
-        this.historyamount_or_coins = historyamount_or_coins;
-        this.historyimage = historyimage;
-    }
-
-    public String getHistoryluckydate() {
-        return historyluckydate;
-    }
-
-    public void setHistoryluckydate(String historyluckydate) {
-        this.historyluckydate = historyluckydate;
-    }
-
-    public String getHistoryluckyname() {
-        return historyluckyname;
-    }
-
-    public void setHistoryluckyname(String historyluckyname) {
-        this.historyluckyname = historyluckyname;
-    }
-
-    public String getHistorypaidby() {
-        return historypaidby;
-    }
-
-    public void setHistorypaidby(String historypaidby) {
-        this.historypaidby = historypaidby;
-    }
-
-    public String getHistoryamount_or_coins() {
-        return historyamount_or_coins;
-    }
-
-    public void setHistoryamount_or_coins(String historyamount_or_coins) {
-        this.historyamount_or_coins = historyamount_or_coins;
->>>>>>> origin/Inhamcode
-    }
-
-    public int getHistoryimage() {
-        return historyimage;
-    }
-
-    public void setHistoryimage(int historyimage) {
-        this.historyimage = historyimage;
     }
 }
